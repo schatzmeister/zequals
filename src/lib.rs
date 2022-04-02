0e655a60ffcaa39050067626d1082a651d3594c6
Schatzmeister <graf.schatzmeister@gmail.com>
2022-04-02 18:40:02 +0200

    Transfer binary into its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
 /// Binary Zequals is the ultimate zequals: Only one significant digit,
 /// and it is always `1` (except for the number `0`, of course).
-mod binary {
-    struct Zequal(i8);
-}
+pub mod binary;
