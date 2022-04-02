d412f939280ad2dfd55f208e7cde731c92028e42
Schatzmeister <graf.schatzmeister@gmail.com>
2022-04-02 19:00:57 +0200

    Add conversion from/to i128

--- src/binary.rs
+++ src/binary.rs
@@ -33,6 +33,13 @@ impl Exponent for u128 {
     }
 }
 
+impl Exponent for i128 {
+    fn exponent(&self) -> i8 {
+        let bits = 127;
+        bits - self.abs().leading_zeros() as i8
+    }
+}
+
 impl From<u128> for Zequal {
     fn from(value: u128) -> Self {
         if value == 0 {
@@ -61,6 +68,40 @@ impl From<Zequal> for u128 {
     }
 }
 
+impl From<i128> for Zequal {
+    fn from(value: i128) -> Self {
+        if value == 0 {
+            Self {
+                exponent: 0,
+                sign: Sign::Zero,
+            }
+        } else if value > 0 {
+            Self {
+                // The sign bit is 0. The maximal number is 2^126,
+                // each further leading zero halves the number.
+                exponent: value.exponent(),
+                sign: Sign::Pos,
+            }
+        } else {
+            Self {
+                // Similar to the `> 0` case but we ignore the sign bit
+                // when counting the leading zeros.
+                exponent: value.exponent(),
+                sign: Sign::Neg,
+            }
+        }
+    }
+}
+
+impl From<Zequal> for i128 {
+    fn from(value: Zequal) -> Self {
+        match value.sign {
+            Sign::Neg => -(1 << value.exponent),
+            Sign::Pos => 1 << value.exponent,
+            Sign::Zero => 0,
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -125,11 +166,30 @@ mod test {
             assert_eq!(u128::from(Zequal::from(power)), power)
         }
 
+        #[test]
+        // Convert 2**n from i128 to Zequals and back to u128.
+        fn test_convert_id_powers_of_two_i128(num in 0..127) {
+            let power: i128 = dbg!(1i128<<dbg!(num));
+            assert_eq!(i128::from(Zequal::from(power)), power)
+        }
+
+        #[test]
         // Convert any number to Zequals and back, and check that the most significant bit
         // is the same
         fn test_convert_id_positive(num: u128) {
             assert_eq!(u128::from(Zequal::from(num)).leading_zeros(), num.leading_zeros())
         }
 
+        #[test]
+        fn test_convert_id_negative_sign(num: i128) {
+            let zeq = i128::from(Zequal::from(num));
+            assert_eq!(zeq.signum(), num.signum());
+        }
+
+        #[test]
+        fn test_convert_id_negative_leading_zeros(num: i128) {
+            let zeq = i128::from(Zequal::from(num));
+            assert_eq!(zeq.abs().leading_zeros(), num.abs().leading_zeros());
+        }
     }
 }
